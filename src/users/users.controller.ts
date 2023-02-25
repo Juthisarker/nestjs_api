3fce34b61b041b5a917bb534363a9d7c6f1de0c5
Juthisarker <tanudas71.21@gmail.com>
2023-02-25 18:23:17 +0600

    added update user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Get, Query, Delete, Patch } from '@nestjs/common';
 import { log } from 'console';
+import { get } from 'http';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-
 
 @Controller('auth')
 export class UsersController {
@@ -10,6 +11,20 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
-    
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+      return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+      return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+      return this.usersService.remove(parseInt(id));
   }
 }
