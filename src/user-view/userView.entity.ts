000baaf80ff0b728d2d6875a378cacfc70120baa
Juthisarker <tanudas71.21@gmail.com>
2023-02-25 23:23:21 +0600

    view added

--- src/user-view/userView.entity.ts
+++ src/user-view/userView.entity.ts
@@ -1,12 +1,22 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-@Entity()
+import { DataSource, ViewColumn, ViewEntity } from "typeorm";
+import { User } from "./../users/user.entity";
+import { Report } from "./../reports/report.entity";
+@ViewEntity({
+    expression: (dataSource: DataSource) => dataSource
+        .createQueryBuilder()
+        .select("user.id", "id")
+        .addSelect("user.email", "email")
+        .addSelect("report.price", "price")
+        .from(User, "user")
+        .leftJoin(Report, "report", "user.id = report.id")
+})
 export class UserView {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @ViewColumn()
+    id: number;
 
-  @Column()
-  price: string;
+    @ViewColumn()
+    source: string;
 
-  @Column()
-  password: string;
+    @ViewColumn()
+    destination: string;
 }
